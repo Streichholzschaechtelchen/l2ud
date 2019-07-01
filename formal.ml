fb5fbf20b65119d2fb0b518e457f9733542bb266
Streichholzschaechtelchen <francois.hublet@polytechnique.edu>
2019-07-01 16:18:24 +0200

    Reductio conditionum

--- formal.ml
+++ formal.ml
@@ -5,6 +5,7 @@ module type Symbol = sig
   val print  : t -> unit
   val lock_labels: < reset: unit -> unit; get: unit -> t >
   val to_string: t -> string
+  val append_int: t -> int -> t
 
 end
               
@@ -314,6 +315,170 @@ module EIDLPMCFG (T: Symbol) (N: Symbol) = struct
   (* Return set L(G) *)
   let language g =
     S.map (Array.map sigma) (linearize g.rules g.start)
+ 
+  (* 3. COMPILATION OF ERULES *)
+
+  type tril = TTrue | TFalse | TUndef
+
+  let (||+) a b =
+    match a, b with
+      TTrue , _
+    | _     , TTrue  -> TTrue
+    | TFalse, TFalse -> TFalse
+    | _     , _      -> TUndef
+                          
+  let (&&+) a b =
+    match a, b with
+      TTrue , TTrue  -> TTrue
+    | TFalse, _
+    | _     , TFalse -> TFalse
+    | _     , _      -> TUndef
+
+  let not_ = function
+      TTrue  -> TFalse
+    | TFalse -> TTrue
+    | TUndef -> TUndef
+                                    
+  module Nontermi = struct
+    type t = nonterm * int
+    let compare = compare
+  end
+                   
+  module NontermiS = Set.Make(Nontermi)
+                          
+  (* Return all term * int pairs used in conditions *)
+  let used_in_cndtn g =
+    let rec process_logic ntis = function
+        LN (n, i) -> NontermiS.add (n, i) ntis
+      | LAnd ls   
+      | LOr ls    -> List.fold_left process_logic ntis ls
+      | LNot l    -> process_logic ntis l
+      | LBool _   -> ntis in
+    let process_rule ntis = function
+        ARule a_r -> ntis
+      | ERule e_r -> process_logic ntis e_r.cndtn in
+    M.fold (fun n rs ntis -> List.fold_left process_rule ntis rs)
+      g.rules NontermiS.empty
+
+  (* Return all fields to mark with emptiness flag *)
+  let fields_to_mark g =
+    let used_ntis = used_in_cndtn g in
+    let rec process_idlexpr args (ntis, new_) = function
+      E      -> (ntis, new_)
+    | T _    -> (ntis, new_)
+    | N (n,i)-> let cat = M.find n args in
+                begin match NontermiS.mem (cat,i) ntis with
+                  true  -> NontermiS.add (cat,i) ntis, NontermiS.add (cat,i) new_
+                | false -> ntis, new_
+                end
+    | C ies
+    | I ies
+    | D ies  -> List.fold_left (process_idlexpr args) (ntis, new_) ies
+    | L ie   -> process_idlexpr args (ntis, new_) ie in
+    let rec process_nti (n, i) ntis =
+      let args incats =
+        List.fold_left (fun args (n, cat) -> M.add n cat args) M.empty incats in
+      let process_rule (ntis, new_) = function
+          ARule a_r -> process_idlexpr (args a_r.incats) (ntis, new_) a_r.exprs.(i)
+        | ERule e_r -> process_idlexpr (args e_r.incats) (ntis, new_) e_r.exprs.(i) in
+      let rs = M.find n g.rules in
+      let ntis, new_ = List.fold_left process_rule (ntis, NontermiS.empty) rs in
+      NontermiS.fold process_nti new_ ntis
+    in NontermiS.fold process_nti used_ntis used_ntis
+
+  (* Compute all new categories associated with old categories *)
+  let compute_tasm g: tril array list M.t =
+    let ftm = fields_to_mark g in
+    let cats: tril array list M.t = M.empty in
+    let arity = function
+        ARule a_r -> Array.length (a_r.exprs)
+      | ERule e_r -> Array.length (e_r.exprs) in
+    let cats = M.fold (fun cat rs cats -> M.add cat [Array.make (arity (List.hd rs)) TUndef] cats) g.rules cats in
+    let process_nti (n,i) cats =
+      M.update n (function None     -> assert false
+                         | Some tas -> Some (List.fold_left (fun l ta -> let tat, taf = ta, Array.copy ta in
+                                                                   tat.(i) <- TTrue; taf.(i) <- TFalse;
+                                                                   tat::taf::l) [] tas))
+        cats
+    in NontermiS.fold process_nti ftm cats
+
+  (* Return new nonterminal encoding new category *)
+  let nonterm_of_cat cat ta =
+    let index = snd (Array.fold_left (fun (i, acc) v
+                                      -> (i * 3, match v with
+                                                   TFalse -> acc
+                                                 | TUndef -> acc + i
+                                                 | TTrue  -> acc + 2 * i))
+                       (1, 0) ta)
+    in N.append_int cat index
+
+  (* Read tril from incats3 list *)
+  let get_trinary_value (n, i) incats3 =
+    let rec aux = function
+          []                         -> assert false
+        | (n', _, ta)::t when n' = n -> ta.(i)
+        | _::t                       -> aux t in
+      aux incats3
+
+  (* Compute new outcat given tril arrays of incats *)
+  let compute_new_outcat cat ies incats3 =
+    let rec process_ie = function
+        E      -> TFalse
+      | T _    -> TTrue
+      | N (n,i)-> get_trinary_value (n,i) incats3
+      | C ies
+      | I ies
+      | D ies  -> List.fold_left (||+) TFalse (List.map process_ie ies)
+      | L ie   -> process_ie ie in
+    nonterm_of_cat cat (Array.map process_ie ies)
+
+  (* Compute logic given tril arrays of incats *)
+  let rec compute_logic incats3 = function
+      LN (n,i)    -> get_trinary_value (n, i) incats3
+    | LAnd ls     -> List.fold_left (&&+) TTrue (List.map (compute_logic incats3) ls)
+    | LOr ls      -> List.fold_left (||+) TFalse (List.map (compute_logic incats3) ls)
+    | LNot l      -> not_ (compute_logic incats3 l)
+    | LBool true  -> TTrue
+    | LBool false -> TFalse
+                          
+  (* EIDL-PMCFG -> IDL-PMCFG *)
+  let develop_grammar g =
+    let tasm  = compute_tasm g in
+    let nsm   = M.mapi (fun cat tas -> List.map (nonterm_of_cat cat) tas) tasm in
+    let rec new_incats_choices acc (n, cat) =
+      let tas = M.find cat tasm in
+      let ns  = M.find cat nsm in
+      List.fold_left2 (fun bdd ta n' ->
+          List.fold_left (fun cee choices -> ((n, n', ta)::choices)::cee)
+            bdd acc) [[]] tas ns in
+    M.fold (fun cat rs rules
+            -> List.fold_left (fun rules -> function
+                     ARule a_r -> let incats3l = List.fold_left new_incats_choices [] a_r.incats in
+                                  let incatsl  = List.map (List.map (fun (n, cat, _) -> (n, cat))) incats3l in
+                                  let outcatl  = List.map (compute_new_outcat cat a_r.exprs) incats3l in
+                                  List.fold_left2 (fun rules incats outcat ->
+                                      M.update outcat (function None ->
+                                                                 Some ([ARule ({ a_r with incats })])
+                                                              | Some l ->
+                                                                 Some ((ARule ({ a_r with incats }))::l)) rules)
+                                                  rules incatsl outcatl
+                   | ERule e_r -> let incats3l = List.fold_left new_incats_choices [] e_r.incats in
+                                  let incatsl  = List.map (List.map (fun (n, cat, _) -> (n, cat))) incats3l in
+                                  let outcatl  = List.map (compute_new_outcat cat e_r.exprs) incats3l in
+                                  let logicl   = List.map (fun incats3 -> compute_logic incats3 e_r.cndtn) incats3l in
+                                  let outlogl  = List.combine outcatl logicl in
+                                  let exprs    = e_r.exprs in
+                                  List.fold_left2 (fun rules incats (outcat, logic) ->
+                                      match logic with
+                                        TTrue -> M.update outcat
+                                                   (function None ->
+                                                              Some ([ARule ({ exprs; incats })])
+                                                           | Some l ->
+                                                              Some ((ARule ({ exprs; incats }))::l)) rules
+                                      | TFalse -> rules
+                                      | TUndef -> assert false)
+                                    rules incatsl outlogl)
+                 rules rs) g.rules M.empty
 
 end              
 (* typo: if p.2 *)
@@ -334,8 +499,24 @@ module MyString = struct
         new_label
     end
   let to_string = fun x -> x
+  let append_int s i = s ^ "(" ^ (string_of_int i) ^ ")"
 
 end
 
 module G = EIDLPMCFG(MyString)(MyString)
              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
